c54b2fca30704a6d92a52632eaf67cda5b7f8444
Nasir Iqbal <nasirpucit@hotmail.com>
2020-10-03 12:47:13 +0100

    Added order context, repositories and setup seed order data in infrastructure project.

---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                // INFO: Run this if using a real database. Used to automaticly migrate docker image of sql server db.
+                await orderContext.Database.MigrateAsync();
+                //orderContext.Database.EnsureCreated();
+
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 5)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(exception.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+                throw;
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>()
+            {
+                new Order() { UserName = "nik", FirstName = "Nasir", LastName = "Iqbal", EmailAddress = "nasir.iqbal@abc.com", AddressLine = "Address1", TotalPrice = 5239 },
+                new Order() { UserName = "sak", FirstName = "Saleem", LastName = "Arslan", EmailAddress ="saleem.arslan@abc.com", AddressLine = "Address2", TotalPrice = 3486 }
+            };
+        }
+    }
+}
